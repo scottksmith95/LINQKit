ca0d7f4a1028f10ced437d5deaf798ff63d3ae71
david_garcia <deivid.garcia.garcia@gmail.com>
2015-04-14 09:25:26 -0500

    Add support for a .NET 3.5 version of the library.

--- src/ExpandableQuery.cs
+++ src/ExpandableQuery.cs
@@ -1,85 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Linq.Expressions;
 using System.Collections;
 using System.Threading;
+#if !NET35
+using System.Data.Entity.Infrastructure;
 using System.Threading.Tasks;
+#endif
 
 namespace LinqKit
 {
-	/// <summary>
-	/// An IQueryable wrapper that allows us to visit the query's expression tree just before LINQ to SQL gets to it.
-	/// This is based on the excellent work of Tomas Petricek: http://tomasp.net/blog/linq-expand.aspx
-	/// </summary>
-	public class ExpandableQuery<T> : IQueryable<T>, IOrderedQueryable<T>, IOrderedQueryable, IDbAsyncEnumerable<T>
-	{
-		readonly ExpandableQueryProvider<T> _provider;
-		readonly IQueryable<T> _inner;
+    /// <summary>
+    /// An IQueryable wrapper that allows us to visit the query's expression tree just before LINQ to SQL gets to it.
+    /// This is based on the excellent work of Tomas Petricek: http://tomasp.net/blog/linq-expand.aspx
+    /// </summary>
+#if NET35
+    public class ExpandableQuery<T> : IQueryable<T>, IOrderedQueryable<T>, IOrderedQueryable
+#else
+    public class ExpandableQuery<T> : IQueryable<T>, IOrderedQueryable<T>, IOrderedQueryable, IDbAsyncEnumerable<T>
+#endif
+    {
+        readonly ExpandableQueryProvider<T> _provider;
+        readonly IQueryable<T> _inner;
 
-		internal IQueryable<T> InnerQuery { get { return _inner; } }			// Original query, that we're wrapping
+        internal IQueryable<T> InnerQuery { get { return _inner; } }			// Original query, that we're wrapping
 
-		internal ExpandableQuery (IQueryable<T> inner)
-		{
-			_inner = inner;
-			_provider = new ExpandableQueryProvider<T> (this);
-		}
+        internal ExpandableQuery(IQueryable<T> inner)
+        {
+            _inner = inner;
+            _provider = new ExpandableQueryProvider<T>(this);
+        }
 
-		Expression IQueryable.Expression { get { return _inner.Expression; } }
-		Type IQueryable.ElementType { get { return typeof (T); } }
-		IQueryProvider IQueryable.Provider { get { return _provider; } }
-		public IEnumerator<T> GetEnumerator () { return _inner.GetEnumerator (); }
-		IEnumerator IEnumerable.GetEnumerator () { return _inner.GetEnumerator (); }
-		public override string ToString() { return _inner.ToString(); }
+        Expression IQueryable.Expression { get { return _inner.Expression; } }
+        Type IQueryable.ElementType { get { return typeof(T); } }
+        IQueryProvider IQueryable.Provider { get { return _provider; } }
+        public IEnumerator<T> GetEnumerator() { return _inner.GetEnumerator(); }
+        IEnumerator IEnumerable.GetEnumerator() { return _inner.GetEnumerator(); }
+        public override string ToString() { return _inner.ToString(); }
 
-		public IDbAsyncEnumerator<T> GetAsyncEnumerator()
-		{
-			var asyncEnumerable = _inner as IDbAsyncEnumerable<T>;
-			if (asyncEnumerable != null)
-				return asyncEnumerable.GetAsyncEnumerator();
-			return new ExpandableDbAsyncEnumerator<T>(_inner.GetEnumerator());
-		}
+#if !NET35
+        public IDbAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            var asyncEnumerable = _inner as IDbAsyncEnumerable<T>;
+            if (asyncEnumerable != null)
+                return asyncEnumerable.GetAsyncEnumerator();
+            return new ExpandableDbAsyncEnumerator<T>(_inner.GetEnumerator());
+        }
 
-		IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
-		{
-			return this.GetAsyncEnumerator();
-		}
-	}
+        IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
+        {
+            return this.GetAsyncEnumerator();
+        }
+#endif
+    }
 
-	class ExpandableQueryProvider<T> : IQueryProvider, IDbAsyncQueryProvider
-	{
-		readonly ExpandableQuery<T> _query;
+#if NET35
+    class ExpandableQueryProvider<T> : IQueryProvider
+#else
+    class ExpandableQueryProvider<T> : IQueryProvider, IDbAsyncQueryProvider
+#endif
+    {
+        readonly ExpandableQuery<T> _query;
 
-		internal ExpandableQueryProvider (ExpandableQuery<T> query)
-		{
-			_query = query;
-		}
+        internal ExpandableQueryProvider(ExpandableQuery<T> query)
+        {
+            _query = query;
+        }
 
-		// The following four methods first call ExpressionExpander to visit the expression tree, then call
-		// upon the inner query to do the remaining work.
+        // The following four methods first call ExpressionExpander to visit the expression tree, then call
+        // upon the inner query to do the remaining work.
 
-		IQueryable<TElement> IQueryProvider.CreateQuery<TElement> (Expression expression)
-		{
-			return new ExpandableQuery<TElement> (_query.InnerQuery.Provider.CreateQuery<TElement> (expression.Expand()));
-		}
+        IQueryable<TElement> IQueryProvider.CreateQuery<TElement>(Expression expression)
+        {
+            return new ExpandableQuery<TElement>(_query.InnerQuery.Provider.CreateQuery<TElement>(expression.Expand()));
+        }
 
-		IQueryable IQueryProvider.CreateQuery (Expression expression)
-		{
-			return _query.InnerQuery.Provider.CreateQuery (expression.Expand());
-		}
+        IQueryable IQueryProvider.CreateQuery(Expression expression)
+        {
+            return _query.InnerQuery.Provider.CreateQuery(expression.Expand());
+        }
 
-		TResult IQueryProvider.Execute<TResult> (Expression expression)
-		{
-			return _query.InnerQuery.Provider.Execute<TResult> (expression.Expand());
-		}
+        TResult IQueryProvider.Execute<TResult>(Expression expression)
+        {
+            return _query.InnerQuery.Provider.Execute<TResult>(expression.Expand());
+        }
 
-		object IQueryProvider.Execute (Expression expression)
-		{
-			return _query.InnerQuery.Provider.Execute (expression.Expand());
-		}
+        object IQueryProvider.Execute(Expression expression)
+        {
+            return _query.InnerQuery.Provider.Execute(expression.Expand());
+        }
 
+#if !NET35
 		public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken)
 		{
 			var asyncProvider = _query.InnerQuery.Provider as IDbAsyncQueryProvider;
@@ -95,5 +108,6 @@ namespace LinqKit
 				return asyncProvider.ExecuteAsync<TResult>(expression.Expand(), cancellationToken);
 			return Task.FromResult(_query.InnerQuery.Provider.Execute<TResult>(expression.Expand()));
 		}
-	}
+#endif
+    }
 }
