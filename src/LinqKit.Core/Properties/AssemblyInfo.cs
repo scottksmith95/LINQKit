4749ebd22a7da8fa0ca98f5cf445b5ad71b210a0
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-10-17 12:52:44 +0200

    Refactor projects

---
+++ src/LinqKit.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LinqKit.Core")]
+[assembly: AssemblyProduct("LinqKit.Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+// [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+#if !(PORTABLE || PORTABLE40)
+[assembly: Guid("fc735db2-5419-4cf3-8aea-8231330a4d85")]
+#endif
---
+++ src/LinqKit.Core/_build nuget.cmd
@@ -0,0 +1,3 @@
+dotnet restore
+dotnet pack -c Release project.json
+pause
