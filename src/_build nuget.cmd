b2130d0f2fc3fa888d7aec5767e7d442d5cae9f2
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-10-31 21:21:36 +0100

    update LinqKit.EntityFramework.Tests.Net452

--- src/_build nuget.cmd
+++
@@ -1,6 +0,0 @@
-dotnet restore
-dotnet pack -c Release LinqKit.Core\project.json
-dotnet pack -c Release LinqKit\project.json
-dotnet pack -c Release LinqKit.EntityFramework\project.json
-dotnet pack -c Release LinqKit.Microsoft.EntityFrameworkCore\project.json
-pause
