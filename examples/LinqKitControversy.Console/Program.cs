c19fd10e7b14841962995406dd6bfc20c4adfdf8
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-11-03 13:26:55 +0100

    update example

--- examples/LinqKitControversy.Console/Program.cs
+++ examples/LinqKitControversy.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using LinqKit;
 using Microsoft.EntityFrameworkCore;
@@ -12,8 +15,14 @@ namespace LinqKitControversy.Console
             var ef6Ctx = new Ef6Context();
             var foo = await ef6Ctx.FooSet.AsExpandableEF().ToListAsync();
 
+            Expression<Func<Bar, bool>> expressionEf6 = (b) => b != null;
+            var resultEf6 = expressionEf6.InvokeEF(null);
+
             var coreCtx = new CoreContext();
             var bar = await coreCtx.BarSet.AsExpandableEFCore().ToListAsync();
+
+            Expression<Func<Bar, bool>> expressionCore = (b) => b != null;
+            var resultEfCore = expressionCore.InvokeEFCore(null);
         }
     }
 }
