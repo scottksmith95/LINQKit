3ce76b8a5cf03589c048e4c7781ff9d320723ae0
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2020-01-23 06:47:22 +0100

    Reproduce LinqKit bug for Net4.7,2 and NetStandard2.0 (#113)
    Co-authored-by: alnintex <53161684+alnintex@users.noreply.github.com>

---
+++ examples/ConsoleAppNet472/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleAppNetCore3Ef3.EntityFrameworkCore;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleAppNet472
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyHotelDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyHotel;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            var c = new MyHotelDbContext(optionsBuilder.Options);
+            c.Database.EnsureCreated();
+
+            var q = c.Guests.AsExpandable();
+
+            var result = q.ToArray();
+            int y = 0;
+        }
+    }
+}
