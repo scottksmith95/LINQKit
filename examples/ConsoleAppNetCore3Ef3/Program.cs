bc9a2cc598a17730fcb0a6c65f897159a6f9be7f
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2020-07-16 20:35:13 +0200

    reproduce issue

--- examples/ConsoleAppNetCore3Ef3/Program.cs
+++ examples/ConsoleAppNetCore3Ef3/Program.cs
@@ -15,15 +15,22 @@ namespace ConsoleAppNetCore3Ef3
             var optionsBuilder = new DbContextOptionsBuilder<MyHotelDbContext>();
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyHotel;Trusted_Connection=True;MultipleActiveResultSets=true");
 
-            var c = new MyHotelDbContext(optionsBuilder.Options);
-            c.Database.EnsureCreated();
+            var context = new MyHotelDbContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+
+            var predicate = PredicateBuilder.New<Guest>().Or(asset => asset.Name.Contains("e"));
+            var predicateResult = context.Guests.AsExpandable()
+                .Where(predicate)
+                .Select(z => z.Name)
+                .ToArray();
+
 
             Expression<Func<Guest, bool>> criteria1 = guest => guest.Name.Contains("af");
             Expression<Func<Guest, bool>> criteria2 = guest => criteria1.Invoke(guest) || guest.Id > 1;
 
             Console.WriteLine($"criteria2 = '{criteria2.Expand()}'");
 
-            var q = c.Guests.AsExpandable().Where(criteria2);
+            var q = context.Guests.AsExpandable().Where(criteria2);
 
             var results = q.ToArray();
             foreach (var result in results)
