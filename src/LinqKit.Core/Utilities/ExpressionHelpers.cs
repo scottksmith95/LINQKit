7950f9c9768dac01ea8ee54cfe8c24293b0b590e
Svyatoslav Danyliv <sdanyliv@gmail.com>
2020-10-21 19:02:24 +0300

    Improved handling target Lambda from Property, Method, Compile() method. (#126)
    * Improved handling target Lambda from Property, Method, Compile() method.
    
    * Code style changes.
    
    * Fixed compilation error.

---
+++ src/LinqKit.Core/Utilities/ExpressionHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace LinqKit.Utilities
+{
+    internal static class ExpressionHelpers
+    {
+        public static object EvaluateExpression(this Expression expr)
+        {
+            if (expr == null)
+            {
+                return null;
+            }
+
+            switch (expr.NodeType)
+            {
+                case ExpressionType.Constant:
+                    return ((ConstantExpression)expr).Value;
+
+                case ExpressionType.MemberAccess:
+                {
+                    var member = (MemberExpression) expr;
+
+                    if (member.Member is FieldInfo field)
+                    {
+                        return field.GetValue(member.Expression.EvaluateExpression());
+                    }
+
+                    if (member.Member is PropertyInfo property)
+                    {
+                        return property.GetValue(member.Expression.EvaluateExpression(), null);
+                    }
+
+                    break;
+                }
+                case ExpressionType.Call:
+                {
+                    var mc = (MethodCallExpression)expr;
+                    var arguments = mc.Arguments.Select(EvaluateExpression).ToArray();
+                    var instance  = mc.Object.EvaluateExpression();
+                    return mc.Method.Invoke(instance, arguments);
+                }
+            }
+
+            return Expression.Lambda(expr).Compile().DynamicInvoke();
+        }
+        
+    }
+}
