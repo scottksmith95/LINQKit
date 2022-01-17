d1247f563ff5fc9157e1c4c51db89dc44c41e2ab
doboczyakos <53097414+doboczyakos@users.noreply.github.com>
2022-01-17 18:30:28 +0100

    #160 Unhandled expression type: 'Index' (#162)
    * #160 Unhandled expression type: 'Index'
    
    * #160 Unhandled expression type: 'Index'

--- tests/LinqKit.Tests.Net452/ExpressionExpanderTests.cs
+++ tests/LinqKit.Tests.Net452/ExpressionExpanderTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace LinqKit.Tests.Net452
@@ -27,6 +30,21 @@ namespace LinqKit.Tests.Net452
             var exp = Expression.UnaryPlus(Expression.Constant(5));
             var executed = exp.Expand().ToString();
             Assert.Equal(exp.ToString(), executed);
+        }
+
+        [Fact]
+        public void ExpressionExpander_Expression_Index()
+        {
+            var listParameter = Expression.Parameter(typeof(List<string>), "l");
+            Expression<Func<List<string>, string>> expression1 = Expression.Lambda<Func<List<string>, string>>(Expression.MakeIndex(
+                    listParameter,
+                    typeof(List<string>).GetProperties().SingleOrDefault(p => p.GetIndexParameters().Length > 0),
+                    new[] { Expression.Constant(0) }),
+                listParameter);
+            Expression<Func<string, string>> expression2 = s => s;
+
+            var executed = Linq.Expr((List<string> l) => expression2.Invoke(expression1.Invoke(l))).Expand().ToString();
+            Assert.Equal(expression1.ToString(), executed);
         }
     }
 }
